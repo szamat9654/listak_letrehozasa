b8f4175a88c69c29d994e11fd3a25a3b8a9d9d0e
szamat9654 <szamate4@gmail.com>
2025-12-16 16:59:02 +0100

    2.2.py

---
+++ 2.2.py
@@ -0,0 +1,13 @@
+szavak = ['ajtó','tojás','Ottó','Tamás', 'tép','Tesla','alma','python']
+
+T = []
+
+lista = []
+
+for i in szavak:
+    if i[0] == "t" or i [0] == "T":
+            T.append(i)
+
+lista = T
+
+print(lista)
